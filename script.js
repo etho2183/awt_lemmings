82e0b85d486f09f62380a85e4a54c741734baea7
Glitch (hello-webpage) <none>
2019-07-07 21:01:32 +0000

    👄🎍 Checkpoint ./index.html:5216928/8 ./script.js:5216928/377

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
+let selectedMode;
 
+function selectMode(gameMode) {
+  selectedMode = gameMode;
+  
+  const levelButtonsContainer = document.getElementById('level-buttons-container');
+  const levelButtons = getLevelSelectionButtons();
+  levelButtons.forEach(button => levelButtonsContainer.appendChild(button))
+}
+
+function getLevelSelectionButtons() {
+  
+}
