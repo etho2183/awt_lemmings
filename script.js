66f34ba06f23f5f73ce46abf433f7dcc35b75031
Glitch (hello-webpage) <none>
2019-07-08 10:46:51 +0000

    💡👁 Checkpoint ./index.html:5216928/62 ./script.js:5216928/268

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
     to your site with Javascript */
 let selectedMode;
 
-function selectMode(gameMode) {
+function showLevelSelector(gameMode) {
   selectedMode = gameMode;
   
   const levelButtonsContainer = document.getElementById('level-buttons-container');
-  const levelButtons = getLevelSelectionButtons();
+  const levelButtons = getLevelSelectionButtons(3);
   levelButtons.forEach(button => levelButtonsContainer.appendChild(button))
 }
 
-function getLevelSelectionButtons() {
+function getLevelSelectionButtons(numberOfLevels) {
   
+}
+
+function createLevelButton(level) {
+  const button = document.createElement('button');
+  button.innerText = 'Level ' + level;
+  button.onclick = alert(level);
+  
+  return button;
 }
