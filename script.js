9704d5166791dbef6f50f48f57ca7999956834cf
Glitch (hello-webpage) <none>
2019-07-08 11:07:49 +0000

    ⛪️💐 Checkpoint ./script.js:5216928/437 ./style.css:5216928/77

--- script.js
+++ script.js
@@ -3,27 +3,32 @@
 let selectedMode;
 
 function showLevelSelector(gameMode) {
+  document.getElementById('mode-buttons-container').style = 'display: none;';
   selectedMode = gameMode;
   
   const levelButtonsContainer = document.getElementById('level-buttons-container');
   const levelButtons = getLevelSelectionButtons(3);
   levelButtons.forEach(button => levelButtonsContainer.appendChild(button));
-  debugger
-  document.getElementById('mode-buttons-container').style = 'display: none;';
 }
 
 function getLevelSelectionButtons(numberOfLevels) {
   const buttonsArray = [];
-  for (let i = 1; i<=numberOfLevels; i++) {
+  for (let level = 1; level <=numberOfLevels; level++) {
     buttonsArray.push(createLevelButton(level));
   }
-  return new Array(numberOfLevels).map((item, index) => createLevelButton(index + 1));
+  
+  return buttonsArray;
 }
 
 function createLevelButton(level) {
   const button = document.createElement('button');
+  button.classList.add('button');
   button.innerText = 'Level ' + level;
-  button.onclick = alert(level);
+  button.addEventListener('click', () => goToLevel(level));
   
   return button;
+}
+
+function goToLevel(level) {
+  alert(level);
 }
